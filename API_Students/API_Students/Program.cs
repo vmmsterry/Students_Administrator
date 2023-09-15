3c4395f56ee2ff264dfd3b8ab6844e934fad6579
VMMSTERRY <vmms.terry@gmail.com>
2023-09-15 01:23:33 -0700

    Swagger

--- API_Students/API_Students/Program.cs
+++ API_Students/API_Students/Program.cs
@@ -62,7 +62,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Lotus.API.Integration v1"));
 }
 
 // Add Cors
